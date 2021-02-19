e6379ac0982394b2b94a522853a78fc2045df2b6
etheriuman <timmy26291125@gmail.com>
2021-02-19 15:38:43 +0800

    add Admin-restaurants

--- src/router/index.js
+++ src/router/index.js
@@ -57,6 +57,16 @@ const routes = [
     component: () => import('../views/User.vue')
   },
   {
+    path: '/admin',
+    exact: true,
+    redirect: 'admin/restaurants'
+  },
+  {
+    path: '/admin/restaurants',
+    name: 'admin-restaurants',
+    component: () => import('../views/AdminRestaurants.vue')
+  },
+  {
     path: '*',
     name: 'not-found',
     component: () => import('../views/NotFound.vue')
