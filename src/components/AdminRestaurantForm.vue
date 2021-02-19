9811bfe9fc07afa4b98f9fc03b43c06981f96c68
etheriuman <timmy26291125@gmail.com>
2021-02-19 21:55:43 +0800

    add image upload feature in restaurants/new page

--- src/components/AdminRestaurantForm.vue
+++ src/components/AdminRestaurantForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form>
+  <form @submit.prevent.stop="handleSubmit">
     <div class="form-group">
       <label for="name">Name</label>
       <input
@@ -83,12 +83,20 @@
 
     <div class="form-group">
       <label for="image">Image</label>
+      <img
+        v-if="restaurant.image"
+        :src="restaurant.image"
+        class="d-block img-thumbnail mb-3"
+        width="200"
+        height="200"
+      >
       <input
         id="image"
         type="file"
         name="image"
         accept="image/*"
         class="form-control-file"
+        @change="handleFileChange"
       >
     </div>
 
@@ -150,6 +158,21 @@ export default {
   methods: {
     fetchCategories() {
       this.categories = dummyData.categories
+    },
+    handleFileChange(e) {
+      const { files } = e.target
+      console.log(files)
+      if (files.length === 0) {
+        this.restaurant.image = ''
+      } else {
+        const imageURL = window.URL.createObjectURL(files[0])
+        this.restaurant.image = imageURL
+      }
+    },
+    handleSubmit(e) {
+      const form = e.target
+      const formData = new FormData(form)
+      this.$emit('after-submit', formData)
     }
   },
   created() {
