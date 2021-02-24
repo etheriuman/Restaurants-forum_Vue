58c38f4b29528563307b9a0b04d5107e33b85417
etheriuman <timmy26291125@gmail.com>
2021-02-24 15:51:28 +0800

    RestaurantsFeeds.vue api init

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -6,5 +6,8 @@ export default {
   getRestaurants({ page, categoryId }) {
     const searchParams = new URLSearchParams({ page, categoryId })
     return apiHelper.get(`/restaurants?${searchParams.toString()}`, {headers: { Authorization: `Bearer ${getToken()}`}})
+  },
+  getFeeds() {
+    return apiHelper.get('/restaurants/feeds', {headers: {Authorization: `Bearer ${getToken()}`}})
   }
 }
