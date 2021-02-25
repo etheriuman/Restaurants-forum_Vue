5a8eb4c05a2642e867aa0b5848322e30dbd9a4b2
etheriuman <timmy26291125@gmail.com>
2021-02-25 22:39:44 +0800

    SignUp.vue api init

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -67,6 +67,7 @@
       <button
         class="btn btn-lg btn-primary btn-block mb-3"
         type="submit"
+        :disabled="isProcessing"
       >
         Submit
       </button>
@@ -87,24 +88,88 @@
 </template>
 
 <script>
+import signUpAPI from './../apis/signUp'
+import { Toast } from './../utils/helpers'
+
 export default {
   data() {
     return {
       name: '',
       email: '',
       password: '',
-      passwordCheck: ''
+      passwordCheck: '',
+      isProcessing: false
     }
   },
   methods: {
-    handleSubmit() {
-      const data = {
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        passwordCheck: this.passwordCheck
+    async handleSubmit() {
+      if (!this.name) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請填寫名稱'
+        })
+        return
       }
-      console.log(JSON.stringify(data))
+      if (!this.email) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請填寫email'
+        })
+        return
+      }
+      if (!this.password) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請填寫密碼'
+        })
+        return
+      }
+      if (!this.passwordCheck) {
+        Toast.fire({
+          icon: 'warning',
+          title: '請填寫密碼確認'
+        })
+        return
+      }
+      if (this.password !== this.passwordCheck) {
+        Toast.fire({
+          icon: 'warning',
+          title: '密碼確認不一致，請再次填寫'
+        })
+        this.passwordCheck = ''
+        return
+      }
+      try {
+        // const form = e.target
+        // console.log(form)
+        // const formData = new FormData(form)
+        // for (let [key, value] of formData.entries()) {
+        //   console.log(`${key}: ${value}`)
+        // }
+        const formData = {
+          name: this.name,
+          email: this.email,
+          password: this.password,
+          passwordCheck: this.passwordCheck
+        }
+        this.isProcessing = true
+        const { data } = await signUpAPI.signUp({ formData })
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+        Toast.fire({
+          icon: 'success',
+          title: '註冊成功'
+        })
+        this.$router.push({name: 'sign-in'})
+      } catch(e) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法註冊帳號，請稍後再試'
+        })
+        this.isProcessing = false
+        console.log(e)
+      }
     }
   }
 }
