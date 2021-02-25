a8d54612e58a093ad7abe26e5b05ebb38b1caaea
etheriuman <timmy26291125@gmail.com>
2021-02-25 17:20:05 +0800

    AdminRestaurant.vue api init

--- src/views/AdminRestaurant.vue
+++ src/views/AdminRestaurant.vue
@@ -44,27 +44,9 @@
 </template>
 <script>
 import { emptyImageFilter } from './../utils/mixins'
-const dummyData = {
-  restaurant: {
-    id: 2,
-    name: 'Mrs. Mckenzie Johnston',
-    tel: '567-714-6131 x621',
-    address: '61371 Rosalinda Knoll',
-    opening_hours: '08:00',
-    description:
-      'Quia pariatur perferendis architecto tenetur omnis pariatur tempore.',
-    image: 'https://loremflickr.com/320/240/food,dessert,restaurant/?random=2',
-    createdAt: '2019-06-22T09:00:43.000Z',
-    updatedAt: '2019-06-22T09:00:43.000Z',
-    CategoryId: 3,
-    Category: {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  }
-}
+import adminAPI from './../apis/admin'
+import { Toast } from './../utils/helpers'
+
 export default {
   name: 'AdminRestaurant',
   mixins: [emptyImageFilter],
@@ -86,19 +68,36 @@ export default {
     const { id: restaurantId } = this.$route.params
     this.fetchRestaurant(restaurantId)
   },
+  beforeRouteUpdate(from, to, next) {
+    const { id } = to.params
+    this.fetchRestaurant(id)
+    next()
+  },
   methods: {
-    fetchRestaurant (restaurantId) {
-      const { restaurant } = dummyData
-      this.restaurant = {
-        ...this.restaurant,
-        id: restaurantId,
-        name: restaurant.name,
-        categoryName: restaurant.Category.name,
-        image: restaurant.image,
-        openingHours: restaurant.opening_hours,
-        tel: restaurant.tel,
-        address: restaurant.address,
-        description: restaurant.description
+    async fetchRestaurant (restaurantId) {
+      try {
+        const response = await adminAPI.restaurants.getDetail({ restaurantId })
+        if (response.statusText !== 'OK') {
+          throw new Error(response.statusText)
+        }
+        const { restaurant } = response.data
+        this.restaurant = {
+          ...this.restaurant,
+          id: restaurantId,
+          name: restaurant.name,
+          categoryName: restaurant.Category? restaurant.Category.name : '未分類',
+          image: restaurant.image,
+          openingHours: restaurant.opening_hours,
+          tel: restaurant.tel,
+          address: restaurant.address,
+          description: restaurant.description
+        }
+      } catch(e) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資訊，請稍後再試'
+        })
+        console.log(e)
       }
     }
   }
