1ae61eeafdb2d0fe5c140608dceb4f02514259e7
etheriuman <timmy26291125@gmail.com>
2021-02-27 01:50:50 +0800

    User.vue api init

--- src/components/UserProfileCard.vue
+++ src/components/UserProfileCard.vue
@@ -18,8 +18,24 @@
           </ul>
           <p>
             <router-link :to="{name: 'user-edit', params: {id: profile.id}}">
-              <button type="submit" class="btn btn-primary">edit</button>
+              <button type="submit" v-if="profile.id === currentUser.id" class="btn btn-primary">edit</button>
             </router-link>
+            <button
+            v-if="profile.isFollowed && profile.id !== currentUser.id"
+            type="submit"
+            class="btn btn-danger"
+            @click.prevent.stop="deleteFollowing(profile.id)"
+            :disabled="isProcessing"
+            >取消追蹤
+            </button>
+            <button
+            v-if="!profile.isFollowed && profile.id !== currentUser.id"
+            type="submit"
+            class="btn btn-primary"
+            @click.prevent.stop="addFollowing(profile.id)"
+            :disabled="isProcessing"
+            >追蹤
+            </button>
           </p>
         </div>
       </div>
@@ -29,11 +45,76 @@
 
 <script>
 import {emptyImageFilter} from '.././utils/mixins'
+import { mapState } from 'vuex'
+import usersAPI from './../apis/users'
+import { Toast } from './../utils/helpers'
+
 export default {
   props: {
-    profile: {
+    initialProfile: {
       type: Object,
       required: true
+    }
+  },
+  data() {
+    return {
+      profile: this.initialProfile,
+      isProcessing: false
+    }
+  },
+  computed: {
+    ...mapState(['currentUser'])
+  },
+  methods: {
+    async addFollowing(userId) {
+      try {
+        this.isProcessing = true
+        const { data } = await usersAPI.addFollowing({ userId })
+        if (data.status !== 'success') {
+          throw new Error(data.status)
+        }
+        this.profile = {
+          ...this.profile,
+          isFollowed: true
+        }
+        this.isProcessing = false
+      } catch(e) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法追蹤使用者，請稍後再試'
+        })
+        this.isProcessing = false
+        console.log(e)
+      }
+    },
+    async deleteFollowing(userId) {
+      try {
+        this.isProcessing = true
+        const { data } = await usersAPI.deleteFollowing({ userId })
+        if (data.status !== 'success') {
+          throw new Error(data.status)
+        }
+        this.profile = {
+          ...this.profile,
+          isFollowed: false
+        }
+        this.isProcessing = false
+      } catch(e) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法退追使用者，請稍後再試'
+        })
+        this.isProcessing = false
+        console.log(e)
+      }
+    }
+  },
+  watch: {
+    initialProfile(data) {
+      this.profile = {
+        ...this.profile,
+        ...data
+      }
     }
   },
   mixins: [emptyImageFilter]
