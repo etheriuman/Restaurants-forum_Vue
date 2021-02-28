b7f1ea962692191c70e55ac16063ff3f61400dec
etheriuman <timmy26291125@gmail.com>
2021-02-28 17:21:43 +0800

    RestaurantsTop.vue Spinner init

--- src/views/RestaurantsTop.vue
+++ src/views/RestaurantsTop.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <h1 class="mt-5">
-      人氣餐廳
-    </h1>
-    <hr>
-    <RestaurantTop v-for="restaurant in restaurants" :key="restaurant.id" :initial-restaurant="restaurant" />
+    <Spinner v-if="isLoading"/>
+    <template v-else>
+      <h1 class="mt-5">
+        人氣餐廳
+      </h1>
+      <hr>
+      <RestaurantTop v-for="restaurant in restaurants" :key="restaurant.id" :initial-restaurant="restaurant" />
+    </template>
   </div>
 </template>
 
 <script>
 import NavTabs from '../components/NavTabs.vue'
 import RestaurantTop from '../components/RestaurantTop.vue'
+import Spinner from './../components/Spinner'
 import restaurantsAPI from './../apis/restaurants'
 import { Toast } from './../utils/helpers'
 
 export default {
   components: {
     NavTabs,
-    RestaurantTop
+    RestaurantTop,
+    Spinner
   },
   data() {
     return {
-      restaurants: []
+      restaurants: [],
+      isLoading: true
     }
   },
   methods: {
@@ -33,12 +39,15 @@ export default {
             if (response.statusText !== 'OK') {
                 throw new Error(response.statusText)
             }
+            this.isLoading = false
             this.restaurants = response.data.restaurants
         } catch(e) {
             Toast.fire({
                 icon: 'error',
                 title: '無法取得人氣餐廳，請稍後再試'
             })
+            this.isLoading = false
+            console.log(e)
         }
     }
   },
