0bf5c59bf7df0a072c917d2d3aec5bbb46291034
etheriuman <timmy26291125@gmail.com>
2021-02-28 17:18:43 +0800

    Restaurants.vue Spinner init

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-    <!-- 餐廳類別標籤 RestaurantsNavPills -->
-    <RestaurantsNavPills :categories="categories" />
-    <div class="row">
-      <!-- 餐廳卡片 RestaurantCard-->
-      <RestaurantCard v-for="restaurant in restaurants" :key="restaurant.id" :initial-restaurant="restaurant" />
-    </div>
+    <Spinner v-if="isLoading"/>
+    <template v-else>
+      <!-- 餐廳類別標籤 RestaurantsNavPills -->
+      <RestaurantsNavPills :categories="categories" />
+      <div class="row">
+        <!-- 餐廳卡片 RestaurantCard-->
+        <RestaurantCard v-for="restaurant in restaurants" :key="restaurant.id" :initial-restaurant="restaurant" />
+      </div>
 
-    <!-- 分頁標籤 RestaurantPagination -->
-    <RestaurantsPagination
-        v-if="totalPage.length > 1"
-        :current-page="currentPage"
-        :total-page="totalPage"
-        :category-id="categoryId"
-        :previous-page="previousPage"
-        :next-page="nextPage"
-      />
+      <!-- 分頁標籤 RestaurantPagination -->
+      <RestaurantsPagination
+          v-if="totalPage.length > 1"
+          :current-page="currentPage"
+          :total-page="totalPage"
+          :category-id="categoryId"
+          :previous-page="previousPage"
+          :next-page="nextPage"
+        />
+    </template>
   </div>
 </template>
 
@@ -25,6 +28,7 @@ import NavTabs from '../components/NavTabs.vue'
 import RestaurantCard from '../components/RestaurantCard.vue'
 import RestaurantsNavPills from '../components/RestaurantsNavPills.vue'
 import RestaurantsPagination from '../components/RestaurantsPagination.vue'
+import Spinner from './../components/Spinner'
 import restaurantsAPI from '../apis/restaurants'
 import { Toast } from '../utils/helpers'
 
@@ -38,14 +42,16 @@ export default {
       currentPage: 1,
       totalPage: [],
       previousPage: undefined,
-      nextPage: undefined
+      nextPage: undefined,
+      isLoading: true
     }
   },
   components: {
     NavTabs,
     RestaurantCard,
     RestaurantsNavPills,
-    RestaurantsPagination
+    RestaurantsPagination,
+    Spinner
   },
   methods: {
       async fetchRestaurants({queryPage, queryCategoryId}) {
@@ -54,6 +60,7 @@ export default {
                 page: queryPage,
                 categoryId: queryCategoryId
             })
+            this.isLoading = false
             console.log(response)
             // 例外處理
             if (response.statusText !== 'OK') {
@@ -81,6 +88,7 @@ export default {
                   icon: 'error',
                   title: '無法取得餐廳資料，請稍後再試'
               })
+              this.isLoading = false
               console.log(e)
           }
       }
