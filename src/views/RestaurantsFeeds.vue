1da85832a51cb11c23c4a769fc8e6213d3ee31a7
etheriuman <timmy26291125@gmail.com>
2021-02-28 17:07:07 +0800

    RestaurantsFeeds.vue Spinner init

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-
-    <h1 class="mt-5">
-      最新動態
-    </h1>
-    <hr>
-    <div class="row">
-      <div class="col-md-6">
-        <h3>最新餐廳</h3>
-        <!-- 最新餐廳 NewestRestaurants -->
-        <NewestRestaurants  :restaurants="restaurants" />
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <h1 class="mt-5">
+        最新動態
+      </h1>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <h3>最新餐廳</h3>
+          <!-- 最新餐廳 NewestRestaurants -->
+          <NewestRestaurants  :restaurants="restaurants" />
+        </div>
+        <div class="col-md-6">
+          <!-- 最新評論 NewestComments-->
+          <NewestComments  :comments="comments" />
+          <h3>最新評論</h3>
+        </div>
       </div>
-      <div class="col-md-6">
-        <!-- 最新評論 NewestComments-->
-        <NewestComments  :comments="comments" />
-        <h3>最新評論</h3>
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -25,6 +27,7 @@
 import NavTabs from '../components/NavTabs.vue'
 import NewestRestaurants from '../components/NewestRestaurants.vue'
 import NewestComments from '../components/NewestComments.vue'
+import Spinner from './../components/Spinner'
 import restaurantsAPI from './../apis/restaurants'
 import { Toast } from './../utils/helpers'
 
@@ -34,12 +37,14 @@ export default {
   components: {
     NavTabs,
     NewestRestaurants,
-    NewestComments
+    NewestComments,
+    Spinner
   },
   data() {
     return {
       restaurants: [],
-      comments: []
+      comments: [],
+      isLoading: true
     }
   },
   methods: {
@@ -50,7 +55,7 @@ export default {
         if (response.statusText !== 'OK') {
           throw new Error(response.statusText)
         }
-
+        this.isLoading = false
         const { restaurants, comments } = response.data
         this.restaurants = restaurants
         this.comments = comments.filter(comment => comment.Restaurant && comment.text)
@@ -59,6 +64,7 @@ export default {
           icon: 'error',
           title: '無法取得餐廳資料，請稍後再試'
         })
+        this.isLoading = false
         console.log(e)
       }
     }
