469eca309ac3be6abb1224f40dd68b9533c2256f
etheriuman <timmy26291125@gmail.com>
2021-02-28 22:06:46 +0800

    fix github pages 404 issue

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? 'restaurants-forum-vue' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/restaurants-forum-vue/' : '/'
 }
